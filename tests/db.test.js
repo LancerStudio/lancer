c6555e13c347ef675a1920a9d3be4fc8508972a2
Gilbert <gilbertbgarza@gmail.com>
2021-02-22 21:19:18 -0600

    Fix test

--- tests/db.test.js
+++ tests/db.test.js
@@ -1,13 +1,13 @@
 const o = require('ospec')
-const db = require('../dist/server/db').connect(':memory:')
+const db = require('../dist/server/lib/db').connect(':memory:')
 
 o.spec('db', () => {
 
   o('initial setup', () => {
-    o( db.get('SELECT COUNT(*) AS result FROM lance_kvs').result ).deepEquals(2)
+    o( db.get('SELECT COUNT(*) AS result FROM lancer_kvs').result ).deepEquals(1)
   })
 
   o('v1_0_0', () => {
-    o( db.get('SELECT COUNT(*) AS result FROM lance_translations').result ).deepEquals(0)
+    o( db.get('SELECT COUNT(*) AS result FROM lancer_translations').result ).deepEquals(0)
   })
 })
