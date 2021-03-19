a2fb07a8e0fb686fafb4f33ebfd6de740750face
Gilbert <gilbertbgarza@gmail.com>
2021-03-19 13:56:12 -0500

    Ensure build dir exists

--- src/server/build.ts
+++ src/server/build.ts
@@ -10,6 +10,7 @@ type Options = {
   goStatic?: boolean
 }
 export async function buildForProduction({ goStatic }: Options = {}) {
+  mkdirSync(buildDir, { recursive: true })
   console.log("Build dir:", buildDir)
 
   const site = siteConfig()
